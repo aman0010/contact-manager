ca9ad9d9244ae912d22b6462be01842a860512d6
aman0010 <shresthaaman416@gmail.com>
2022-03-22 17:18:46 +0545

    backend - added columns in user table

--- backend/src/migrations/20170107202211_create_users_table.js
+++ backend/src/migrations/20170107202211_create_users_table.js
@@ -5,14 +5,12 @@
  * @returns {Promise}
  */
 export function up(knex) {
-  return knex.schema.createTable('users', table => {
+  return knex.schema.createTable('users', (table) => {
     table.increments();
-    table
-      .timestamp('created_at')
-      .notNull()
-      .defaultTo(knex.raw('now()'));
+    table.timestamp('created_at').notNull().defaultTo(knex.raw('now()'));
     table.timestamp('updated_at').notNull();
-    table.string('name').notNull();
+    table.string('email').unique().notNull();
+    table.string('password').notNull();
   });
 }
 
